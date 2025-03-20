2614eedd5af3849d2709e885781db50dc71df3d0
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-03-20 16:09:23 -0300

    Salas Funcionando

--- Dante's Revolver/Assets/Scripts/Room.cs
+++ Dante's Revolver/Assets/Scripts/Room.cs
@@ -3,10 +3,10 @@ using TMPro;
 
 public class Room : MonoBehaviour
 {
-    public TMP_Text name;
+    public TMP_Text r_name;
 
     public void JoinRoom()
     {
-        GameObject.Find("CreateAndJoin").GetComponent<CreateAndJoin>().JoinRoomInList(name.text);
+        GameObject.Find("CreateAndJoin").GetComponent<CreateAndJoin>().JoinRoomInList(r_name.text);
     }
 }
--- Dante's Revolver/Assets/Scripts/RoomList.cs
+++ Dante's Revolver/Assets/Scripts/RoomList.cs
@@ -6,14 +6,28 @@ using System.Collections.Generic;
 public class RoomList : MonoBehaviourPunCallbacks
 {
     public GameObject roomPrefab;
+    public GameObject[] allRooms;
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
+        for(int i = 0; i<allRooms.Length; i++)
+        {
+            if (allRooms[i] != null)
+            {
+                Destroy(allRooms[i]);
+            }
+        }
+
         for (int i = 0; i < roomList.Count; i++)
         {
-            print (roomList[i].Name);
-            GameObject Room = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
-            Room.GetComponent<Room>().name.text = roomList[i].Name;
+            if (roomList[i].IsOpen && roomList[i].IsVisible && roomList[i].PlayerCount >= 1)
+            {
+                GameObject Room = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
+                Room.GetComponent<Room>().r_name.text = roomList[i].Name;
+
+                allRooms[i] = Room;
+            }
         }
     }
 }
+//https://www.youtube.com/watch?v=GipaiFVyQt0&list=PL_l4pPo5eSc2nHazxm_VjIrURNdWcWQpd&index=4&t=51s&ab_channel=BudGames
