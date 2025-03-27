e2df78dcb293dec58d61a8157084ab1e7cb53fa6
trelepero <bernardocitadin@gmail.com>
2025-03-27 16:04:42 -0300

    CameraFirstPersonController_WIP

--- Dante's Revolver/Assets/Scripts/ServerSpawn.cs
+++ Dante's Revolver/Assets/Scripts/ServerSpawn.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using Photon.Pun;
+using System.Collections.Generic;
 
 public class ServerSpawn : MonoBehaviour
 {
+    public List<GameObject> playerList;
     private void Start()
     {
         if(PhotonNetwork.IsConnected && PhotonNetwork.CurrentRoom != null)
         {
-            PhotonNetwork.Instantiate("FirstPersonController", new Vector3(0, 1, 0), Quaternion.identity);
+            playerList.Add(PhotonNetwork.Instantiate("FirstPersonController", new Vector3(0, 1, 0), Quaternion.identity));
+            playerList[playerList.Count - 1].GetComponentInChildren<Camera>().enabled = true;
         }
     }
 }
