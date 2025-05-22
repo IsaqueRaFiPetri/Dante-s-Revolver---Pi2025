2458f7ad6275bba246349ba40c20a179aa2dde3e
trelepero <bernardocitadin@gmail.com>
2025-05-22 15:45:37 -0300

    WallrunWIP

--- Dante's Revolver/Assets/Scripts/Player/PlayerCam.cs
+++ Dante's Revolver/Assets/Scripts/Player/PlayerCam.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerCam : MonoBehaviour
 {
     public float sensX;
-    public float sensY;
+    //public float sensY;
 
     public Transform orientation;
     public Transform camHolder;
@@ -21,11 +21,11 @@ public class PlayerCam : MonoBehaviour
     {
         // get mouse input
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        //float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
 
-        xRotation -= mouseY;
+        //xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         // rotate cam and orientation
