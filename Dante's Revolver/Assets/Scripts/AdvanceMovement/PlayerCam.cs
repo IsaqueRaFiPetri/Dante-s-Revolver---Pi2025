c2d05ac2eb24aa8336dffa4ea01592017cf55b35
trelepero <bernardocitadin@gmail.com>
2025-06-03 09:06:08 -0300

    Update PlayerCam.cs

--- Dante's Revolver/Assets/Scripts/AdvanceMovement/PlayerCam.cs
+++ Dante's Revolver/Assets/Scripts/AdvanceMovement/PlayerCam.cs
@@ -17,25 +17,27 @@ public class PlayerCam : MonoBehaviourPunCallbacks
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        if (!photonView.IsMine)
+        {
+            Destroy(this);
+        }
     }
 
     private void Update()
     {
-        if (photonView.IsMine)
-        {
-            // get mouse input
-            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-            yRotation += mouseX;
+        yRotation += mouseX;
 
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            // rotate cam and orientation
-            camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-        }
+        // rotate cam and orientation
+        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 
     public void DoFov(float endValue)
