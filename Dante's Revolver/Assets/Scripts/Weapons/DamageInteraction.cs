ff0390555b41c96b6fbb02b84607c1b6ba20374d
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-06-03 08:56:18 -0300

    Update DamageInteraction.cs

--- Dante's Revolver/Assets/Scripts/Weapons/DamageInteraction.cs
+++ Dante's Revolver/Assets/Scripts/Weapons/DamageInteraction.cs
@@ -6,7 +6,7 @@ public interface IDamaging
 }
 public class DamageInteraction : MonoBehaviour, IDamaging
 {
-    //[SerializeField] protected WeaponStats weaponsStats;
+    [SerializeField] protected WeaponStats weaponsStats;
 
     public void DoDamage(GameObject target)
     {
