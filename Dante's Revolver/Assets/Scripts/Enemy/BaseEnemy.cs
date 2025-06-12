766cfffdadcdb458141d0d77976bd6a529a90335
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-06-12 10:07:03 -0300

    EnemyMoveConcluded

--- Dante's Revolver/Assets/Scripts/Enemy/BaseEnemy.cs
+++ Dante's Revolver/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -13,45 +13,45 @@ public class BaseEnemy : MonoBehaviour
         body = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
-    {
-        Debug.Log(body.linearVelocity);
-
-        moveDirection = Vector3.zero;
-
-        if (Vector3.Distance(player.position, this.transform.position) < 25)
-        {
-            body.linearVelocity = transform.forward * moveSpeed;
-
-            print("following");
-            Vector3 direction = player.position - this.transform.position;
-            direction.y = 0;
-
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-        }
-        else if (Vector3.Distance(player.position, this.transform.position) >= 25 && Vector3.Distance(player.position, this.transform.position) <= 100)
-        {
-            body.linearVelocity = transform.forward * (moveSpeed * 5);
-
-            Vector3 direction = player.position - this.transform.position;
-            direction.y = 0;
-
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-            print("trying to escape");
-        }
-        else
-        {
-            body.linearVelocity = transform.forward * moveSpeed * 0;
-            print("bye");
-        }
-    }
-
     void OnTriggerEnter(Collider collision)
     {
         if (collision.GetComponent<IPlayable>() != null)
         {
             print("entered");
             player = collision.transform;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (player == null) return;
+
+        moveDirection = Vector3.zero;
+        
+        float direction = Vector3.Distance(player.position, this.transform.position);
+        
+        Vector3 dir = player.position - this.transform.position;
+        dir.y = 0;
+
+        if (direction < 15)
+        {
+            body.linearVelocity = transform.forward * moveSpeed;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(dir), 0.1f);
+
+            print("following");
+        }
+        else if (direction >= 15 && direction < 40)
+        {
+            body.linearVelocity = transform.forward * (moveSpeed * 5);
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(dir), 0.1f);
+
+            print("trying to escape");
+        }
+        else
+        {
+            body.linearVelocity = transform.forward * moveSpeed * 0;
+            print("bye");
         }
     }
 }
