8929b43393d3c8d027120f0545c90f90f912b89d
trelepero <bernardocitadin@gmail.com>
2025-07-02 13:48:17 -0300

    Moveset_04 + RocketLauncherWIP

--- Dante's Revolver/Assets/RocketLauncher.cs
+++ Dante's Revolver/Assets/RocketLauncher.cs
@@ -4,16 +4,28 @@ using UnityEngine.Events;
 public class RocketLauncher : MonoBehaviour
 {
     [SerializeField] UnityEvent OnStep;
+    public float radius = 15.0F;
+    public float power = 80.0F;
     private void OnCollisionEnter(Collision collision)
     {
         print("hit");
         if (collision.gameObject.TryGetComponent(out Rigidbody rb))
         {
-            print("collide hit");
-            rb.AddForce(transform.forward * 10, ForceMode.Impulse);
-            rb.AddForce(transform.up * 20, ForceMode.Impulse);
-
+            print("collide");
+            Knockback();
             OnStep.Invoke();
+        }
+    }
+    void Knockback()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rigidBody = hit.GetComponent<Rigidbody>();
+            print("explosion");
+            if (rigidBody != null)
+                rigidBody.AddExplosionForce(power, explosionPos, radius, 3.0F);
         }
     }
 }
