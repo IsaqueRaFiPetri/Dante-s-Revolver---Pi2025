763240e61e2a27d3119f678961cdb01532599b9b
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-07-10 16:36:38 -0300

    Update Angel.cs

--- Dante's Revolver/Assets/Scripts/General/Angel.cs
+++ Dante's Revolver/Assets/Scripts/General/Angel.cs
@@ -1,7 +1,47 @@
 using UnityEngine;
 using Photon.Pun;
+using System.Collections.Generic;
 
 public class Angel : MonoBehaviourPunCallbacks
 {
+    List<Transform> players;
+    Transform player;
 
+
+    private void Start()
+    {
+        FollowPlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        FollowPlayer();
+        RotateTowardsPlayer();
+
+    }
+    void FollowPlayer()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject p in allPlayers)
+        {
+            float dist = Vector3.Distance(transform.position, p.transform.position);
+            if (dist < closestDistance)
+            {
+                closestDistance = dist;
+                player = p.transform;
+            }
+            
+        }
+    }
+    void RotateTowardsPlayer()
+    {
+        if (player != null)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 5f * Time.fixedDeltaTime);
+        }
+    }
 }
