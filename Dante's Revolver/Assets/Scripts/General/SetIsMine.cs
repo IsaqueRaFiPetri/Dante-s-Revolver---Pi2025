cef187e89a615163ed5a47faeca88170cd25508c
trelepero <bernardocitadin@gmail.com>
2025-07-17 08:48:15 -0300

    SetIsMineFixed

--- Dante's Revolver/Assets/Scripts/General/SetIsMine.cs
+++ Dante's Revolver/Assets/Scripts/General/SetIsMine.cs
@@ -1,15 +1,28 @@
 using Photon.Pun;
 using UnityEngine;
 
+public enum SetIsMineEnum
+{
+    Renderer, Object
+}
 public class SetIsMine : MonoBehaviourPunCallbacks
 {
     [SerializeField] bool hasToBeMine;
     [SerializeField] SkinnedMeshRenderer skinRender;
+    [SerializeField] SetIsMineEnum setIsMineEnum;
     private void Awake()
     {
-        if(photonView.IsMine == hasToBeMine)
+        if (photonView.IsMine == hasToBeMine)
         {
-            skinRender.enabled = false;
+            switch (setIsMineEnum)
+            {
+                case SetIsMineEnum.Renderer:
+                    skinRender.enabled = false;
+                    break;
+                case SetIsMineEnum.Object:
+                    gameObject.SetActive(false);
+                    break;
+            }
         }
     }
 }
