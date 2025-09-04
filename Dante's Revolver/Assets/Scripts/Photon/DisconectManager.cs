b9b80d7ec30c6d17cbd4291d0f4e90cc6a2da4bb
IsaqueRaFiPetri <irfpetri@gmail.com>
2025-09-04 20:59:33 -0300

    EntregaXandeConcluida

--- Dante's Revolver/Assets/Scripts/Photon/DisconectManager.cs
+++ Dante's Revolver/Assets/Scripts/Photon/DisconectManager.cs
@@ -3,15 +3,51 @@ using Photon.Pun;
 using UnityEngine.SceneManagement;
 using Photon.Realtime;
 using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class DisconectManager : MonoBehaviourPunCallbacks
 {
     public static DisconectManager instance;
     public static HashSet<int> intentionallyLeftPlayers = new HashSet<int>();
 
+    [SerializeField] UnityEvent OnPause, OnUnpause;
+    bool isPaused;
+
     private void Awake()
     {
         instance = this;
+    }
+    void SetCursor()
+    {
+        if (isPaused)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        Cursor.visible = isPaused;
+    }
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnPause.Invoke();
+            SetPaused(true);
+            SetCursor();
+        }
+        if (context.canceled)
+        {
+            OnUnpause.Invoke();
+            SetPaused(false);
+            SetCursor();
+        }
+    }
+    bool SetPaused(bool _paused)
+    {
+        return isPaused = _paused;
     }
 
     public void Disconnect(string sceneName)
