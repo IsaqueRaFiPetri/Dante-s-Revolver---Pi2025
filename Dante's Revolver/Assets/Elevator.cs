95538f225c408b3ea76592c2e84c425ef5431302
trelepero <bernardocitadin@gmail.com>
2025-09-25 16:34:39 -0300

    PlayerStatsChanges + ElevatorAtt

--- Dante's Revolver/Assets/Elevator.cs
+++ Dante's Revolver/Assets/Elevator.cs
@@ -1,21 +1,23 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
+using Photon.Pun;
 
 public class Elevator : MonoBehaviour, IKillable
 {
-    [SerializeField] GameObject _elevatorObj;
     [SerializeField] UnityEvent OnClick;
+    [SerializeField] Transform[] _elevatorPositions;
     public GameObject GetGameObject()
     {
         return gameObject;
     }
+    [PunRPC]
     public void TakeDamage(int damage)
     {
         OnClick.Invoke();
     }
     public void SetPos(float _setPos)
     {
-        transform.DOMoveZ(_setPos, .25f);
+        transform.DOLocalMoveZ(_setPos, .25f);
     }
 }
--- Dante's Revolver/Assets/ElevatorObj.cs
+++ Dante's Revolver/Assets/ElevatorObj.cs
@@ -1,13 +1,27 @@
+using Photon.Pun;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ElevatorObj : MonoBehaviour
 {
     [SerializeField] bool isInitialElevator;
+    [SerializeField] List<GameObject> _objInsideElevator;
     private void OnBecameInvisible()
     {
         if (isInitialElevator)
         {
             Destroy(gameObject);
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PhotonView _pv))
+        {
+
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        
     }
 }
