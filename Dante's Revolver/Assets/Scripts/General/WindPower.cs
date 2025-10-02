905fd970b48e744ca7ae9fa1c311fe947ca5728f
isqu3 <isq.rafipe@gmail.com>
2025-10-02 09:39:14 -0300

    2FloorWIP

---
+++ Dante's Revolver/Assets/Scripts/General/WindPower.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+using UnityEngine;
+
+public class WindPower : MonoBehaviour
+{
+    [SerializeField] float windForce;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.TryGetComponent(out Rigidbody _rb))
+        {
+            _rb.AddForce(transform.forward * windForce, ForceMode.Acceleration);
+        }
+    }
+}
