a751e0285d08ed7b5c5d35f30f373e227d16b62d
trelepero <bernardocitadin@gmail.com>
2025-10-02 15:37:30 -0300

    RevolverAtt_5

--- Dante's Revolver/Assets/Scripts/Weapons/Revolver/Revolver.cs
+++ Dante's Revolver/Assets/Scripts/Weapons/Revolver/Revolver.cs
@@ -17,7 +17,7 @@ public class Revolver : DamageInteraction
     float reloadCooldown;
 
     RevolverMoves revolverMoves;
-    [SerializeField] PlayerController _playerController;
+    [SerializeField] IKillable _player;
     [SerializeField] Transform bulletHolder;
     [SerializeField] List<Image> bulletImage;
     [SerializeField] UnityEvent OnShoot;
@@ -34,6 +34,7 @@ public class Revolver : DamageInteraction
     }
     private void Start()
     {
+        _player = GetComponentInParent<IKillable>();
         shootCooldown = weaponsStats.shootCooldown;
         reloadCooldown = weaponsStats.reloadCooldown;
         revolverMoves = GetComponent<RevolverMoves>();
@@ -79,18 +80,7 @@ public class Revolver : DamageInteraction
 
         if (Physics.Raycast(ray.origin, ray.direction, out hit, weaponsStats.maxDistance))
         {
-            if(hit.collider.TryGetComponent(out PlayerController _controller))
-            {
-                if(_controller == _playerController)
-                {
-                    print("yourself=================================================================================");
-                    return;
-                }
-                else
-                {
-                    print("other Player=========================================================================");
-                }
-            }
+
             if (hit.collider.TryGetComponent(out WeakPoint headshot))
             {
                 print("hit headshot");
@@ -99,9 +89,12 @@ public class Revolver : DamageInteraction
             }
             if (hit.collider.TryGetComponent(out IKillable _target))
             {
-                ShootParticle(bloodParticle.gameObject, hit);
-                ShootParticle(damageParticle.gameObject, hit);
-                DoDamage(_target);
+                if (_target != _player)
+                {
+                    ShootParticle(bloodParticle.gameObject, hit);
+                    ShootParticle(damageParticle.gameObject, hit);
+                    DoDamage(_target);
+                }
             }
             if (!hit.collider.TryGetComponent(out ILifeable lifePoint))
             {
