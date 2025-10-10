a514b0d8cbc55fc423c665ddfd2875310e7747e4
trelepero <bernardocitadin@gmail.com>
2025-10-10 10:32:12 -0300

    OcclusionCulling + CollisionsRemadeTut

--- Dante's Revolver/Assets/Lava.cs
+++ Dante's Revolver/Assets/Lava.cs
@@ -1,13 +1,28 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
-    [SerializeField] float _inpuseForce;
+    [SerializeField] float _inpulseForce;
+    [SerializeField] int _lavaDamage;
+    [SerializeField] bool _isTeleportLava;
+    [SerializeField] Transform _revivePoint;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.TryGetComponent(out Rigidbody _rb))
         {
-            _rb.AddForce(transform.up * _inpuseForce, ForceMode.Impulse);
+            if (_isTeleportLava)
+            {
+                _rb.transform.DOMove(_revivePoint.position, .25f).SetEase(Ease.InCubic);
+            }
+            else
+            {
+                _rb.AddForce(transform.up * _inpulseForce, ForceMode.Impulse);
+                if(_rb.TryGetComponent(out IKillable _iKillable))
+                {
+                    _iKillable.TakeDamage(_lavaDamage);
+                }
+            }
         }
     }
 }
