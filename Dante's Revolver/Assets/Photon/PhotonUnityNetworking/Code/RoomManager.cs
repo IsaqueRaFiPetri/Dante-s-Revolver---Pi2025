9e662a2c1066abc2f0e5ad9da3b514b8a0e6bebd
trelepero <bernardocitadin@gmail.com>
2025-10-31 08:38:53 -0300

    TestFPS

---
+++ Dante's Revolver/Assets/Photon/PhotonUnityNetworking/Code/RoomManager.cs
@@ -0,0 +1,91 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+
+    [Header("Scene and Player Settings")]
+    [SerializeField] private string loadingSceneName;
+    [SerializeField] GameObject playerPrefabName;
+    [SerializeField] private Transform[] spawnPoints;
+
+    [Header("Timer Reference")]
+
+    private bool playerSpawned = false;
+    private bool timerStarted = false;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else { Destroy(gameObject); return; }
+
+        if (PhotonNetwork.InRoom)
+            TrySpawnAndStartTimer();
+    }
+
+    private void Start()
+    {
+        if (PhotonNetwork.InRoom && !playerSpawned)
+            TrySpawnAndStartTimer();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        TrySpawnAndStartTimer();
+    }
+
+    private void TrySpawnAndStartTimer()
+    {
+        if (!PhotonNetwork.InRoom)
+            return;
+
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        if (playerSpawned)
+            return;
+
+        if (spawnPoints == null || spawnPoints.Length == 0)
+            return;
+
+        int index = PhotonNetwork.LocalPlayer.ActorNumber % spawnPoints.Length;
+        Transform point = spawnPoints[index];
+
+        if (point == null)
+            return;
+
+        PhotonNetwork.Instantiate(playerPrefabName.name, point.position, point.rotation);
+        playerSpawned = true;
+    }
+
+    public void LeaveRoom()
+    {
+        if (PhotonNetwork.InRoom)
+            PhotonNetwork.LeaveRoom();
+        else
+            SceneManager.LoadScene(loadingSceneName);
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene(loadingSceneName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        SceneManager.LoadScene(loadingSceneName);
+    }
+
+    public string GetRoomName()
+    {
+        if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom != null)
+            return PhotonNetwork.CurrentRoom.Name;
+        return string.Empty;
+    }
+}
