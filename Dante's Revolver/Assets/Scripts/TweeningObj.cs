d0a5fa2b6db4695849a0a2af52afa9ea89a2e872
trelepero <bernardocitadin@gmail.com>
2025-11-12 09:40:26 -0300

    Floor7Att

--- Dante's Revolver/Assets/Scripts/TweeningObj.cs
+++ Dante's Revolver/Assets/Scripts/TweeningObj.cs
@@ -9,19 +9,58 @@ public class TweeningObj : MonoBehaviour
 {
     [SerializeField] TweenType _tweenType;
     [SerializeField] Vector3 _finalVector;
+    [SerializeField] Vector3 _initialVector;
     [SerializeField] float _duration;
-    public void ApplyTweenTo(Transform _objTransform)
+    [SerializeField] bool _gatherVectorInStart;
+    private void Start()
+    {
+        if (_gatherVectorInStart)
+        {
+            GatherInitialVector();
+        }
+    }
+    public void GatherInitialVector()
     {
         switch (_tweenType)
         {
             case TweenType.Position:
-                _objTransform.DOMove(_finalVector, _duration);
+                _initialVector = transform.position;
                 break;
             case TweenType.Scale:
-                _objTransform.DOScale(_finalVector, _duration);
+                _initialVector = transform.localScale;
                 break;
             case TweenType.Rotation:
-                _objTransform.DOLocalRotate(_finalVector, _duration);
+                _initialVector = transform.localEulerAngles;
+                break;
+        }
+    }
+    public void ApplyTweenTo()
+    {
+        switch (_tweenType)
+        {
+            case TweenType.Position:
+                transform.DOMove(_finalVector, _duration);
+                break;
+            case TweenType.Scale:
+                transform.DOScale(_finalVector, _duration);
+                break;
+            case TweenType.Rotation:
+                transform.DOLocalRotate(_finalVector, _duration);
+                break;
+        }
+    }
+    public void ResetTween()
+    {
+        switch (_tweenType)
+        {
+            case TweenType.Position:
+                transform.DOMove(_initialVector, _duration);
+                break;
+            case TweenType.Scale:
+                transform.DOScale(_initialVector, _duration);
+                break;
+            case TweenType.Rotation:
+                transform.DOLocalRotate(_initialVector, _duration);
                 break;
         }
     }
