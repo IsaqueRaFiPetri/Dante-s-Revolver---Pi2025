221e715844bf75c38e30106039ce5156420758fc
trelepero <bernardocitadin@gmail.com>
2025-11-25 10:45:37 -0300

    FixedRevive

--- Dante's Revolver/Assets/Revive.cs
+++ Dante's Revolver/Assets/Revive.cs
@@ -2,7 +2,7 @@ using Photon.Pun;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Revive : MonoBehaviour
+public class Revive : MonoBehaviourPunCallbacks
 {
     [SerializeField] bool _isPlayerIn;
     [SerializeField] bool _isSoulIn;
@@ -12,11 +12,12 @@ public class Revive : MonoBehaviour
         if(other.TryGetComponent(out PhantomMode _playerPhantom))
         {
             _phantomPlayer = _playerPhantom;
-            _isSoulIn = true;
+            photonView.RPC("SetBoolTrue", RpcTarget.AllBuffered, _isSoulIn);
         }
         if(other.TryGetComponent(out PlayerController _playerObj))
         {
             _isPlayerIn = true;
+            photonView.RPC("SetBoolTrue", RpcTarget.AllBuffered, _isPlayerIn);
         }
         DetectPlayers();
     }
@@ -25,18 +26,30 @@ public class Revive : MonoBehaviour
         if (other.TryGetComponent(out PhantomMode _playerPhantom))
         {
             _phantomPlayer = null;
-            _isSoulIn = false;
+            photonView.RPC("SetBoolFalse", RpcTarget.AllBuffered, _isSoulIn);
         }
         if (other.TryGetComponent(out PlayerController _playerObj))
         {
-            _isPlayerIn = false;
+            photonView.RPC("SetBoolFalse", RpcTarget.AllBuffered,_isPlayerIn);
         }
     }
+    [PunRPC]
+    public bool SetBoolTrue(bool _bool)
+    {
+        return _bool = true;
+    }
+    [PunRPC]
+    public bool SetBoolFalse(bool _bool)
+    {
+        return _bool = false;
+    }
+
     void DetectPlayers()
     {
         if(_isPlayerIn && _isSoulIn)
         {
             _phantomPlayer.isInReviveArea(new Vector3(transform.position.x, transform.position.y + 5, transform.position.z));
+            _phantomPlayer = null;
             PhotonNetwork.Destroy(gameObject);
         }
     }
