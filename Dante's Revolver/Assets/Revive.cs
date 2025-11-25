2a6b16a89d36cb2ceefb42e21b71a930aed1e2f8
BernardoCitadin <bernardocitadin@gmail.com>
2025-11-24 21:04:52 -0300

    Update Revive.cs

--- Dante's Revolver/Assets/Revive.cs
+++ Dante's Revolver/Assets/Revive.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 
 public class Revive : MonoBehaviour
 {
-    [SerializeField] List<PhotonView> _players;
+    [SerializeField] bool _isPlayerIn;
+    [SerializeField] bool _isSoulIn;
+    PhantomMode _phantomPlayer;
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out PhantomMode _playerPhantom))
         {
-            _players.Add(_playerPhantom.gameObject.GetPhotonView());
+            _phantomPlayer = _playerPhantom;
+            _isSoulIn = true;
         }
         if(other.TryGetComponent(out PlayerController _playerObj))
         {
-            _players.Add(_playerObj.photonView);
+            _isPlayerIn = true;
         }
-        if (_players.Count >= 2)
-        {
-            _playerPhantom.isInReviveArea(new Vector3(transform.position.x, transform.position.y + 5, transform.position.z));
-            print("2 people");
-            _players.Clear();
-        }
+        DetectPlayers();
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent(out PhantomMode _playerPhantom))
         {
-            _players.Remove(_playerPhantom.gameObject.GetPhotonView());
+            _phantomPlayer = null;
+            _isSoulIn = false;
         }
         if (other.TryGetComponent(out PlayerController _playerObj))
         {
-            _players.Remove(_playerObj.photonView);
+            _isPlayerIn = false;
+        }
+    }
+    void DetectPlayers()
+    {
+        if(_isPlayerIn && _isSoulIn)
+        {
+            _phantomPlayer.isInReviveArea(new Vector3(transform.position.x, transform.position.y + 5, transform.position.z));
         }
     }
 }
