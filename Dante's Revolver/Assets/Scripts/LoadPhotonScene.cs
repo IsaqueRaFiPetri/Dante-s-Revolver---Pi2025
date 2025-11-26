c2585e4747a364ded963f8395da26137be4441a1
BernardoCitadin <bernardocitadin@gmail.com>
2025-11-26 20:02:19 -0300

    SceneOrderAtt

--- Dante's Revolver/Assets/Scripts/LoadPhotonScene.cs
+++ Dante's Revolver/Assets/Scripts/LoadPhotonScene.cs
@@ -1,10 +1,19 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadPhotonScene : MonoBehaviour
 {
+    [SerializeField] bool isPhotonScene;
     public void LoadScene(string _sceneName)
     {
-        PhotonNetwork.LoadLevel(_sceneName);
+        if (isPhotonScene)
+        {
+            PhotonNetwork.LoadLevel(_sceneName);
+        }
+        else
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
     }
 }
