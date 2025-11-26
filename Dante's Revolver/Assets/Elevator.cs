3ce0e27e5e8acefe74778ea81a8bfc836239db0d
BernardoCitadin <bernardocitadin@gmail.com>
2025-11-26 20:10:53 -0300

    Update Elevator.cs

--- Dante's Revolver/Assets/Elevator.cs
+++ Dante's Revolver/Assets/Elevator.cs
@@ -8,13 +8,9 @@ public class Elevator : MonoBehaviour, IKillable
     [SerializeField] UnityEvent OnClick;
     [SerializeField] Transform[] _elevatorPositions;
     [SerializeField] ElevatorObj _elevatorObj;
-    [SerializeField] Vector2 _teleportPos;
     private void Start()
     {
         _elevatorObj = GetComponentInParent<ElevatorObj>();
-
-        _teleportPos.x = _elevatorPositions[1].position.x - _elevatorPositions[0].position.x;
-        _teleportPos.y = _elevatorPositions[1].position.z - _elevatorPositions[0].position.z;
     }
     public GameObject GetGameObject()
     {
@@ -31,7 +27,7 @@ public class Elevator : MonoBehaviour, IKillable
         for (int i = 0; i < _elevatorObj.GetPlayers().Count; i++)
         {
             _elevatorObj.GetPlayers()[i].GetComponent<Collider>().enabled = false;
-            _elevatorObj.GetPlayers()[i].MovePosition(new Vector3 (_elevatorObj.GetPlayers()[i].transform.position.x + _teleportPos.x, _elevatorObj.GetPlayers()[i].transform.position.y, _elevatorObj.GetPlayers()[i].transform.position.z + _teleportPos.y));
+            _elevatorObj.GetPlayers()[i].MovePosition(new Vector3(_elevatorPositions[1].position.x, _elevatorPositions[1].position.y, _elevatorPositions[1].position.z));
             _elevatorObj.GetPlayers()[i].GetComponent<Collider>().enabled = true;
         }
     }
