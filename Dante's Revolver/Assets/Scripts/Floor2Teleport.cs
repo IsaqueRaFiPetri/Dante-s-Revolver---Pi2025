76facc3ca54c41bca5399cd71aecb88d2d9b7ae0
BernardoCitadin <bernardocitadin@gmail.com>
2025-11-26 06:45:46 -0300

    AddedTeleport to floo2

--- Dante's Revolver/Assets/Scripts/Floor2Teleport.cs
+++ Dante's Revolver/Assets/Scripts/Floor2Teleport.cs
@@ -3,5 +3,14 @@ using UnityEngine.Events;
 
 public class Floor2Teleport : MonoBehaviour
 {
-
+    [SerializeField] UnityEvent OnCollision;
+    [SerializeField] Transform _position;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlayerMovementAdvanced _pma))
+        {
+            _pma.transform.position = _position.position;
+            OnCollision.Invoke();
+        }
+    }
 }
