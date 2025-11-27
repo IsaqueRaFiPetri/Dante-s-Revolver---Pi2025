c00ed1dc98874d3cce1f9ba5f762a8d35b2400ff
BernardoCitadin <bernardocitadin@gmail.com>
2025-11-26 21:26:23 -0300

    AttPhotonView

--- Dante's Revolver/Assets/Scripts/General/HoleCounter.cs
+++ Dante's Revolver/Assets/Scripts/General/HoleCounter.cs
@@ -1,9 +1,11 @@
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class HoleCounter : MonoBehaviour
 {
     [SerializeField] int layerToFind;
+    [SerializeField] Layer2Manager _layerManager;
     [SerializeField] UnityEvent OnEnemyEntered;
     
     private void OnTriggerEnter(Collider collision)
@@ -11,6 +13,7 @@ public class HoleCounter : MonoBehaviour
         if(collision.gameObject.layer == layerToFind)
         {
             OnEnemyEntered.Invoke();
+            _layerManager.GetComponent<PhotonView>().RPC("DetectEnemyQuantity", RpcTarget.AllBuffered);
         }
     }
 }
--- Dante's Revolver/Assets/Scripts/General/Layer2Manager.cs
+++ Dante's Revolver/Assets/Scripts/General/Layer2Manager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
+using Photon.Pun;
 
 public class Layer2Manager : MonoBehaviour
 {
     [SerializeField] int _minEnemyQuantity, _maxEnemyQuantity;
     [SerializeField] UnityEvent OnQuantityReached;
+    [PunRPC]
     public void DetectEnemyQuantity()
     {
         if (_minEnemyQuantity >= _maxEnemyQuantity)
