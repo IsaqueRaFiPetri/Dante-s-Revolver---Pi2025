fccfeba44dc165abdfdf2f61f7620d5ca2083c94
BernardoCitadin <bernardocitadin@gmail.com>
2025-11-27 06:41:27 -0300

    AddedEnd

--- Dante's Revolver/Assets/Cage.cs
+++ Dante's Revolver/Assets/Cage.cs
@@ -7,6 +7,7 @@ public class Cage : DamageInteraction
     bool canFall;
     [SerializeField] UnityEvent OnExitTrigger;
     [SerializeField] Rigidbody _rb;
+    [SerializeField] bool _canKill;
     Vector3 _startPos;
     [SerializeField] Vector3 _finalPos;
     private void Start()
@@ -28,17 +29,22 @@ public class Cage : DamageInteraction
     {
         if(collision.gameObject.TryGetComponent(out IKillable _iKillable))
         {
-            print(_iKillable.GetHashCode());
-            DoDamage(_iKillable);
+            if (_canKill)
+            {
+                print(_iKillable.GetHashCode());
+                DoDamage(_iKillable);
+            }
         }
     }
     public void Fall()
     {
         _rb.DOMoveY(_finalPos.y, .25f).SetEase(Ease.InQuart).onComplete = GetUp;
+        _canKill = true;
     }
     void GetUp()
     {
         _rb.DOMoveY(_startPos.y, 4f).SetEase(Ease.Linear);
+        _canKill = false;
     }
     void DetectCanFall()
     {
